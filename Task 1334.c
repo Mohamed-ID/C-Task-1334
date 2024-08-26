a685ac9688b760205032a68eecfdb7866a91c421
ID 272 <Mohamed-ID@Github.com>
2024-08-26 19:46:01 +0100

    Add Task 2

--- Task 1334.c
+++ Task 1334.c
@@ -4,10 +4,26 @@ void ft_putchar(int c) {
     write(1, &c, 1);
 }
 
+// Task 2
+void ft_print_alphabet(void) {
+    char alphabet[] = "a b c d e f j h i g k l m n o p q r s t u v w x y z";
+    int length = sizeof(alphabet)/sizeof(alphabet[0]);
+    int i = 0;
+    while(i < length) {
+        write(1, &alphabet[i], 1);
+        i++;
+    }
+}
+
 int main() {
-    ft_putchar(65);
-    write(1, "\nHello world!", 13);
+    ft_putchar('A');
+    // ################
+    write(1, "\n", 1);
+    // ################
+    ft_print_alphabet();
     return 0;
 }
 
-// Task 2
+
+
+
